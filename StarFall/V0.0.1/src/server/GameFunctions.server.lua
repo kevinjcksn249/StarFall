5fa84cf46f3e882e348beba8edc37eeb5ddb481e
kevinjcksn249 <kevinjcksn249@gmail.com>
2022-05-08 22:41:21 -0400

    Selene config
    Changed Selene configuration to work with project

--- StarFall/V0.0.1/src/server/GameFunctions.server.lua
+++ StarFall/V0.0.1/src/server/GameFunctions.server.lua
@@ -1,19 +1,33 @@
 local scriptService = game:GetService("ServerScriptService")
+local workspace = game:GetService("Workspace")
+local serverStorage = game:GetService("ServerStorage")
 local fntns = scriptService.GameFunctions
+
 
 -- Bindable functions
 -----------------------------------------------------------------
-function fntns.SpawnPlayer.OnInvoke(player, ship)
-	player.PlayerGui.LoadingGui.FadeIn:InvokeClient(player)
 
-	local s = ship:Clone()
+-- Called by player gui's spawn script
+function fntns.SpawnPlayer.OnInvoke(playerObj, shipObj)
+	-- Fade player's loading gui in
+	playerObj.PlayerGui.LoadingGui.FadeIn:InvokeClient(playerObj)
+
+	-- Spawn in the player's ship
+	local s = shipObj:Clone()
 	s.Parent = workspace
-	s.NameTag.gui.lbl.Text = player.Name
-	s:MoveTo(player.PlayerGui.ShipSelectGui.Destination.Value.Position)
-	player.Character = s
-	workspace.GameFunctions.SetCamera:Invoke(player, s.Engine)
+
+	-- Give ship player's name
+	s.NameTag.gui.lbl.Text = playerObj.Name
+
+	-- Move the ship to the spawn location decided on by the gui
+	s:MoveTo(playerObj.PlayerGui.ShipSelectGui.Destination.Value.Position)
+	playerObj.Character = s
+	workspace.GameFunctions.SetCamera:Invoke(playerObj, s.Engine)
 	local c = game.ServerStorage["Ship Controlls"]:Clone()
-	c.Parent = player.Backpack
-	player.PlayerGui.LoadingGui.FadeOut:InvokeClient(player)
+	c.Parent = playerObj.Backpack
+	playerObj.PlayerGui.LoadingGui.FadeOut:InvokeClient(playerObj)
+end
+
+function fntns.BuildShip.OnInvoke(shipName)
 
 end
